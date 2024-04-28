7f3bac792b6e7a2bb1e11945c2ea6424c1c5d9c5
Fiorela Sirias <61396158+Sirias2412@users.noreply.github.com>
2024-04-28 00:41:27 -0600

    Arreglo en lista de amigos

--- src/services/list_follow_users.services.js
+++ src/services/list_follow_users.services.js
@@ -16,13 +16,17 @@ class ListFollowUsersService {
         console.log(res.ListFollowUsers);
         return res;
     }
-    
-    async find() {
-        const res = await models.ListFollowUsers.findAll();
+
+    async find(id) {
+        console.log(id)
+        const res = await models.ListFollowUsers.findAll({
+            where: {
+                id_user:id,
+            },
+          });
         return res;
     }
     
-
     async findOne(id){
         const res = await models.ListFollowUsers.findByPk(id);
         return res;

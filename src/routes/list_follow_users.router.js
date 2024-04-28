24e79e0e65e7102e31b7130c319a9dd214201dad
Fiorela Sirias <61396158+Sirias2412@users.noreply.github.com>
2024-04-28 01:51:37 -0600

    Arreglos para eliminar de la lista de amigos

--- src/routes/list_follow_users.router.js
+++ src/routes/list_follow_users.router.js
@@ -4,5 +4,6 @@ const followController = require('../controllers/list_follow_users.controller');
 
 router
     .post('/', followController.create)
-    .get('/:id', followController.get);
+    .get('/:id', followController.get)
+    .delete('/:id/:id2', followController._delete);
 module.exports = router;

0dc4f240b2e3673ae58cef3638db41b5637b7487
Fiorela Sirias <61396158+Sirias2412@users.noreply.github.com>
2024-04-28 00:11:13 -0600

    Creación de estructura para lista de seguidores

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const usersRouter = require('./users.router');
+const followRouter = require('./list_follow_users.router');
 
 function routerApi(app){
     const router = express.Router();
     app.use('/api/v1',router);
     router.use('/users',usersRouter);
+    router.use('/follow',followRouter);
 }
 
 module.exports = routerApi;
